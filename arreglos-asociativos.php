359cadf6cf5ff6a52b5e5c37f0c43ebb5d10887b
alexander <alexander5243188@yahoo.es>
2024-03-23 18:08:49 -0400

    se creo una lista, dentro de otra

---
+++ arreglos-asociativos.php
@@ -0,0 +1,23 @@
+<?php 
+
+$descripcion = [
+    "broaster" => [
+        "idea" => "diversion",
+        "numero" => 22,
+    ],
+    "refrescos" => [
+        "idea" => "joda", 
+        "numero" => 24,
+    ], 
+    "caldos" => [
+        "idea" => "apetitosa",
+        "numero" => 26,
+    ], 
+    "verduras" => [
+        "idea" => "hermosa",
+        "numero" => 24,
+    ], 
+];
+
+echo "Imagino que lo mejor seria {$descripcion["broaster"]["idea"]} ya que tiene {$descripcion["broaster"]["numero"]} " ;
+?>
